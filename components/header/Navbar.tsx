2a0d50d55119c1330524fd1cfe1f41e0c25387c5
David Blanco <david.lucas@maeztra.com>
2023-06-28 09:43:55 -0300

    header spacing mobile

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -20,7 +20,7 @@ function Navbar({
       {/* Mobile Version */}
       <div
         style={{ height: navbarHeight }}
-        class="md:hidden flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-6 gap-2"
+        class="md:hidden flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-3 gap-2"
       >
         <Buttons variant="menu" />
 
