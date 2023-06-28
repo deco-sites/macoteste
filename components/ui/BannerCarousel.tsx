0efd5cc1a8287aba5ece05c0050bc36a3df7c151
Lucas <nyw@pop-os.localdomain>
2023-06-28 12:19:01 -0300

    trocando h4 por span no pagamentos footer e adicionando tamanhos de banners principais

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -7,176 +7,199 @@ import { useId } from "preact/hooks";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
 export interface Banner {
-  /** @description desktop otimized image */
-  desktop: LiveImage;
-  /** @description mobile otimized image */
-  mobile: LiveImage;
-  /** @description Image's alt text */
-  alt: string;
-  action?: {
-    /** @description when user clicks on the image, go to this link */
-    href: string;
-    /** @description Image text title */
-    title: string;
-    /** @description Image text subtitle */
-    subTitle: string;
-    /** @description Button label */
-    label: string;
-  };
+    /** @description desktop otimized image */
+    desktop: LiveImage;
+    /** @description mobile otimized image */
+    mobile: LiveImage;
+    /** @description Image's alt text */
+    alt: string;
+    action?: {
+        /** @description when user clicks on the image, go to this link */
+        href: string;
+        /** @description Image text title */
+        title: string;
+        /** @description Image text subtitle */
+        subTitle: string;
+        /** @description Button label */
+        label: string;
+    };
+}
+
+type Sizes = {
+    mobileHeight?: number;
+    mobileWidth?: number;
+    desktopHeight?: number;
+    desktopWidth?: number;
 }
 
 export interface Props {
-  images?: Banner[];
-  /**
-   * @description Check this option when this banner is the biggest image on the screen for image optimizations
-   */
-  preload?: boolean;
-  /**
-   * @title Autoplay interval
-   * @description time (in seconds) to start the carousel autoplay
-   */
-  interval?: number;
+    images?: Banner[];
+    /**
+     * @description Check this option when this banner is the biggest image on the screen for image optimizations
+     */
+    preload?: boolean;
+    /**
+     * @title Autoplay interval
+     * @description time (in seconds) to start the carousel autoplay
+     */
+    interval?: number;
+    /**
+     * @title Tamanho das imagens
+     */
+    sizes?:{
+        /**
+         * @title altura imagem mobile em px
+         * @default 565
+         */
+        mobileHeight?: number;
+        /**
+         * @title largura imagem mobile em px
+         * @default 390
+         */
+        mobileWidth?: number;
+        /**
+         * @description altura imagem deskto pem px
+         * @default 411
+         */
+        desktopHeight?: number;
+        /**
+         * @default 1440
+         */
+        desktopWidth?: number;
+    }
+
 }
 
-function BannerItem({ image, lcp }: { image: Banner; lcp?: boolean }) {
-  const {
-    alt,
-    mobile,
-    desktop,
-    action,
-  } = image;
+function BannerItem({ image, lcp,sizes }: { image: Banner; lcp?: boolean,sizes:Sizes | undefined }) {
+    const { alt, mobile, desktop, action, } = image;
 
-  return (
-    <a
-      href={action?.href ?? "#"}
-      aria-label={action?.label}
-      class="relative overflow-y-hidden w-full"
-    >
-      <Picture preload={lcp}>
-        <Source
-          media="(max-width: 767px)"
-          fetchPriority={lcp ? "high" : "auto"}
-          src={mobile}
-          width={390}
-          height={565}
-        />
-        <Source
-          media="(min-width: 768px)"
-          fetchPriority={lcp ? "high" : "auto"}
-          src={desktop}
-          width={1440}
-          height={411}
-        />
+    return (
+        <a href={action?.href ?? "#"} aria-label={action?.label} class="relative overflow-y-hidden w-full" >
+            <Picture preload={lcp}>
+                <Source
+                    media="(max-width: 767px)"
+                    fetchPriority={lcp ? "high" : "auto"}
+                    src={mobile}
+                    width={sizes?.mobileHeight || 390}
+                    height={sizes?.mobileWidth || 565}
+                />
+                <Source
+                    media="(min-width: 768px)"
+                    fetchPriority={lcp ? "high" : "auto"}
+                    src={desktop}
+                    width={sizes?.desktopWidth || 1440}
+                    height={sizes?.desktopHeight || 411}
+                />
 
-        <img
-          class=" w-full h-auto"
-          loading={lcp ? "eager" : "lazy"}
-          src={desktop}
-          alt={alt}
-        />
-      </Picture>
-      {action && (
-        <div class="absolute top-0 bottom-0 m-auto left-0 right-0 sm:right-auto sm:left-[12%] max-h-min max-w-[235px] flex flex-col gap-4 p-4 rounded glass">
-          <span class="text-6xl font-medium text-base-100">
-            {action.title}
-          </span>
-          <span class="font-medium text-xl text-base-100">
-            {action.subTitle}
-          </span>
-          <Button class="glass">{action.label}</Button>
-        </div>
-      )}
-    </a>
-  );
+                <img
+                    class=" w-full h-auto"
+                    loading={lcp ? "eager" : "lazy"}
+                    src={desktop}
+                    alt={alt}
+                />
+            </Picture>
+            {action && (
+                <div class="absolute top-0 bottom-0 m-auto left-0 right-0 sm:right-auto sm:left-[12%] max-h-min max-w-[235px] flex flex-col gap-4 p-4 rounded glass">
+                    <span class="text-6xl font-medium text-base-100">
+                        {action.title}
+                    </span>
+                    <span class="font-medium text-xl text-base-100">
+                        {action.subTitle}
+                    </span>
+                    <Button class="glass">{action.label}</Button>
+                </div>
+            )}
+        </a>
+    );
 }
 
 function Dots({ images, interval = 0 }: Props) {
-  return (
-    <>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
+    return (
+        <>
+            <style
+                dangerouslySetInnerHTML={{
+                    __html: `
           @property --dot-progress {
             syntax: '<percentage>';
             inherits: false;
             initial-value: 0%;
           }
           `,
-        }}
-      />
-      <ul class="carousel justify-center col-span-full gap-4 z-10 row-start-4">
-        {images?.map((_, index) => (
-          <li class="carousel-item">
-            <Slider.Dot index={index}>
-              <div class="py-5">
-                <div
-                  class="w-16 sm:w-20 h-0.5 rounded group-disabled:animate-progress bg-gradient-to-r from-base-100 from-[length:var(--dot-progress)] to-[rgba(255,255,255,0.4)] to-[length:var(--dot-progress)]"
-                  style={{ animationDuration: `${interval}s` }}
-                />
-              </div>
-            </Slider.Dot>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+                }}
+            />
+            <ul class="carousel justify-center col-span-full gap-4 z-10 row-start-4">
+                {images?.map((_, index) => (
+                    <li class="carousel-item">
+                        <Slider.Dot index={index}>
+                            <div class="py-5">
+                                <div
+                                    class="w-16 sm:w-20 h-0.5 rounded group-disabled:animate-progress bg-gradient-to-r from-base-100 from-[length:var(--dot-progress)] to-[rgba(255,255,255,0.4)] to-[length:var(--dot-progress)]"
+                                    style={{ animationDuration: `${interval}s` }}
+                                />
+                            </div>
+                        </Slider.Dot>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 function Buttons() {
-  return (
-    <>
-      <div class="flex items-center justify-center z-10 
+    return (
+        <>
+            <div class="flex items-center justify-center z-10 
       absolute top-1/2 translate-y-[-15px] left-4
       ">
-        <Slider.PrevButton class="btn btn-circle glass">
-          <Icon
-            class="text-base-100"
-            size={20}
-            id="ChevronLeft"
-            strokeWidth={3}
-          />
-        </Slider.PrevButton>
-      </div>
-      <div class="flex items-center justify-center z-10 
+                <Slider.PrevButton class="btn btn-circle glass">
+                    <Icon
+                        class="text-base-100"
+                        size={20}
+                        id="ChevronLeft"
+                        strokeWidth={3}
+                    />
+                </Slider.PrevButton>
+            </div>
+            <div class="flex items-center justify-center z-10 
             absolute top-1/2 translate-y-[-15px] right-4
       ">
-        <Slider.NextButton class="btn btn-circle glass">
-          <Icon
-            class="text-base-100"
-            size={20}
-            id="ChevronRight"
-            strokeWidth={3}
-          />
-        </Slider.NextButton>
-      </div>
-    </>
-  );
+                <Slider.NextButton class="btn btn-circle glass">
+                    <Icon
+                        class="text-base-100"
+                        size={20}
+                        id="ChevronRight"
+                        strokeWidth={3}
+                    />
+                </Slider.NextButton>
+            </div>
+        </>
+    );
 }
 
-function BannerCarousel({ images, preload, interval }: Props) {
-  const id = useId();
+function BannerCarousel({ images, preload, interval,sizes }: Props) {
+    const id = useId();
 
-  return (
-    <div
-      id={id}
-      class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px] relative"
-    >
-      <Slider class="carousel carousel-center w-full col-span-full row-span-full scrollbar-none gap-6">
-        {images?.map((image, index) => (
-          <Slider.Item index={index} class="carousel-item w-full">
-            <BannerItem image={image} lcp={index === 0 && preload} />
-          </Slider.Item>
-        ))}
-      </Slider>
-      {images?.length != undefined && images?.length > 1 ? <Buttons /> : null}
+    return (
+        <div
+            id={id}
+            class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px] relative"
+        >
+            <Slider class="carousel carousel-center w-full col-span-full row-span-full scrollbar-none gap-6">
+                {images?.map((image, index) => (
+                    <Slider.Item index={index} class="carousel-item w-full">
+                        <BannerItem image={image} sizes={sizes} lcp={index === 0 && preload} />
+                    </Slider.Item>
+                ))}
+            </Slider>
+            {images?.length != undefined && images?.length > 1 ? <Buttons /> : null}
 
-      {images?.length != undefined && images?.length > 1
-        ? <Dots images={images} interval={interval} />
-        : null}
+            {images?.length != undefined && images?.length > 1
+                ? <Dots images={images} interval={interval} />
+                : null}
 
-      <SliderJS rootId={id} interval={interval && interval * 1e3} infinite />
-    </div>
-  );
+            <SliderJS rootId={id} interval={interval && interval * 1e3} infinite />
+        </div>
+    );
 }
 
 export default BannerCarousel;
